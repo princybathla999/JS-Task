aad7fdec4ce24da087cf57b60a4ca92ea2f8a12d
Alpha Blog <princybathla999@yahoo.com>
2017-08-04 16:49:04 -0400

    Some odd,even.divisible task in JS

---
+++ app.js
@@ -0,0 +1,32 @@
+console.log("Printing all number between -10 and 19...");
+var counter = -10
+while (counter <= 19) {
+    console.log(counter);
+    counter++;
+}
+
+console.log("Printing all even numbers between 10 and 40...");
+var counter = 10
+while (counter <= 40) {
+    if (counter % 2 == 0) {
+        console.log(counter);
+    }
+    counter++;
+}
+
+console.log("Printing all odd numbers between 300 and 333...");
+var counter = 300
+while (counter <= 333) {
+    if (counter % 2 != 0) {
+        console.log(counter);
+    }
+    counter ++;
+}
+console.log("Printing all numbers divisible by 5 AND 3 between 5 and 50..");
+var counter = 5
+while (counter <= 50) {
+    if ((counter % 5 == 0)&& (counter % 3 == 0)) {
+        console.log(counter);
+    }
+    counter ++;
+}
